75ade0b48866d1bbd201d1d0a17de9efb0eff351
Jose Quintero <Cuellojose29@gmail.com>
2023-11-19 23:28:35 -0500

    carrito fixed

--- app/Models/Pedidos.php
+++ app/Models/Pedidos.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Pedidos extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'producto_id',
+        'cantidad',
+        'precio',
+        'carrito',
+    ];
 }
